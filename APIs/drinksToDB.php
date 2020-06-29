5b4566ba3cfe973dcb3c8b2f83755e4de441450a
Ryan Welch <ryanwelch.nsa@gmail.com>
2020-06-28 22:49:11 -0400

    Attempting to fill table with drink ID and drink Names

--- APIs/drinksToDB.php
+++ APIs/drinksToDB.php
@@ -28,12 +28,25 @@ function cocktailsToDB(){
     
     $arr = [];
 
+    $db = new mysqli('localhost','root','','raptor');
+
     for ($count=0; $count < 100; $count++) { 
         $drinkId = $json['drinks'][$count]['idDrink'];
         $drinkName = $json['drinks'][$count]['strDrink'];
 
         $arr[$drinkId] = $drinkName;
-        // This is where the drinkID and drink name will be sent to the database
+
+        $check = $db->query("SELECT COUNT(*) FROM objectif");
+
+        if ($check->num_rows == 0 || $check->fetch_field() == 0){
+            echo "Table is empty. Filling table with data";
+            // This is where the drinkID and drink name will be sent to the database
+            $sql = 'INSERT INTO 'drinkInfo' (drinkID, drinkName) VALUES ($drinkId, $drinkName)';
+
+        }else{
+            echo "Table is not empty. Fetching data";
+            print_r($arr); // Do this for now due to time constraint
+        }
     }
 
     $endTime = microtime(true);
@@ -71,12 +84,26 @@ function ordinaryDrinksToDB(){
     
     $arr = [];
 
+    $db = new mysqli('localhost','root','','raptor');
+
     for ($count=0; $count < 100; $count++) { 
         $drinkId = $json['drinks'][$count]['idDrink'];
         $drinkName = $json['drinks'][$count]['strDrink'];
 
         $arr[$drinkId] = $drinkName;
-        // This is where the drinkID and drink name will be sent to the database
+        
+
+        $check = $db->query("SELECT COUNT(*) FROM objectif");
+
+        if ($check->num_rows == 0 || $check->fetch_field() == 0){
+            echo "Table is empty. Filling table with data";
+            // This is where the drinkID and drink name will be sent to the database
+            $sql = 'INSERT INTO 'drinkInfo' (drinkID, drinkName) VALUES ($drinkId, $drinkName)';
+
+        }else{
+            echo "Table is not empty. Fetching data";
+            print_r($arr); // Do this for now due to time constraint
+        }
     }
 
     $endTime = microtime(true);
