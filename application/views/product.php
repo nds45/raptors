b75cf409edfd17b3325dedfa1d180d17244a90ec
srp244 <55101123+srp244@users.noreply.github.com>
2020-07-26 21:19:42 -0400

    Add files via upload

---
+++ application/views/product.php
@@ -0,0 +1,238 @@
+  <div class="vc_empty_space" id="product"><span class="vc_empty_space_inner"></span></div>
+    <div class="widget-text-heading  heading-style4 text-center">
+        <div class="subtitle">
+            Our Products
+        </div>
+
+    </div>
+    <div class="wpb-container vc_row wpb_row vc_row-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/1.jfif" alt="Dairy Farm">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>GUINNESS</h4>
+                                    </div>
+                                    <p class="description">$ 2.99</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/2.jfif" alt="Organic Food">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>DARK HORSE</h4>
+                                    </div>
+                                    <p class="description">$ 150</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/4.jpg" alt="Farm Tours">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>BLUE LABEL SPECIAL EDITION</h4>
+                                    </div>
+                                    <p class="description">$ 350</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/amrut.jpg" alt="Dairy Farm">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>AMRUT</h4>
+                                    </div>
+                                    <p class="description">$ 165</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/2.jpg" alt="Organic Food">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>DALMORE</h4>
+                                    </div>
+                                    <p class="description">$ 159</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/3.jpg" alt="Farm Tours">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>BlueLabel</h4>
+                                    </div>
+                                    <p class="description">$ 200</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/ipa.jfif" alt="Dairy Farm">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>IPA</h4>
+                                    </div>
+                                    <p class="description">$ 2.55</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/2.jpeg" alt="Organic Food">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>MILLER</h4>
+                                    </div>
+                                    <p class="description">$ 2.56</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/3.jpeg" alt="Farm Tours">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>BLUEMOON</h4>
+                                    </div>
+                                    <p class="description">$ 2.60</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/4.jpeg" alt="Dairy Farm">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>BUDWEISER</h4>
+                                    </div>
+                                    <p class="description">$ 2</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/red.jpeg" alt="Organic Food">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>APOTHIC RED</h4>
+                                    </div>
+                                    <p class="description">$ 20</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wpb_column vc_column_container vc_col-sm-4">
+                    <div class="vc_column-inner ">
+                        <div class="wpb_wrapper">
+                            <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                                <div class="fbox-image">
+                                    <img src="./images/2.jfif" alt="Farm Tours">
+                                </div>
+                                <div class="fbox-content">
+                                    <div class="fbox-body">
+                                        <h4>DarkHorse Cab</h4>
+                                    </div>
+                                    <p class="description">$ 14</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="wpb_column vc_column_container vc_col-sm-4">
+                <div class="vc_column-inner ">
+                    <div class="wpb_wrapper">
+                        <div class="feature-box feature-box-v6  separator_align_center clearfix ">
+                            <div class="fbox-image">
+                                <img src="./images/3.jpeg" alt="Farm Tours">
+                            </div>
+                            <div class="fbox-content">
+                                <div class="fbox-body">
+                                    <h4>BlueMoon12pk</h4>
+                                </div>
+                                <p class="description">$ 20</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </div>
+    
